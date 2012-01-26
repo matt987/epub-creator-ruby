e0c1e7188eeb116e2384c463bccca8403d33e123
Carlos Emanuel Mathiasen <matt987@mathiasen.com>
2012-01-26 14:53:54 -0300

    added get_xhtml_file

--- epub.rb
+++ epub.rb
@@ -6,11 +6,15 @@ require 'nokogiri'
 class Epub
 
    def initialize(filename)
+     begin
       @zip = Zip::ZipFile.open(filename)
       @opf = opf
       @metadata = metadata
       @cover_image = cover_image
       @xhtmls = xhtmls
+     rescue
+       raise "File not fund"
+     end
    end
 
    def opf
@@ -83,11 +87,16 @@ class Epub
      files_urls = []
      @opf.at_css("package manifest").children.each do |i| 
        if i['media-type'] == "application/xhtml+xml" 
-	 files_urls << i['href']
+	 files_urls << @base_path + i['href'].to_s
        end
      end
      files_urls
    end
-
+   
+   def get_xhtml_file(path)
+     puts path
+     @zip.get_input_stream(path) {|f| f.read}
+   end
+   
 
 end
